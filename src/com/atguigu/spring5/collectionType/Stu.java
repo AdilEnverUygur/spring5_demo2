0a5f85f1f79092475ea7b670f210545966ff738c
Adil <Adiljan.anwar@gmail.com>
2022-08-29 15:57:19 +0300

    Initial commit

---
+++ src/com/atguigu/spring5/collectionType/Stu.java
@@ -0,0 +1,41 @@
+package com.atguigu.spring5.collectionType;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+public class Stu {
+
+    //1 数组类型属性
+    private String[] courses;
+
+    //2 List集合类型属性
+    private List<String> list;
+
+    //3 Map集合类型属性
+    private Map<String,String> maps;
+
+    //4 Set集合类型属性
+    private Set<String> sets;
+
+    public void setCourses(String[] courses) {
+        this.courses = courses;
+    }
+    public void setList(List<String> list) {
+        this.list = list;
+    }
+    public void setMaps(Map<String, String> maps) {
+        this.maps = maps;
+    }
+    public void setSets(Set<String> sets) {
+        this.sets = sets;
+    }
+
+    public void test(){
+        System.out.println(Arrays.toString(courses));
+        System.out.println(list);
+        System.out.println(maps);
+        System.out.println(sets);
+    }
+}
---
+++ src/com/atguigu/spring5/testDemo/TestSpring5Demo1.java
@@ -0,0 +1,18 @@
+package com.atguigu.spring5.testDemo;
+
+import com.atguigu.spring5.collectionType.Stu;
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring5Demo1 {
+
+    @Test
+    public void testCollection(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean1.xml");
+        Stu stu = context.getBean("stu", Stu.class);
+        stu.test();
+
+    }
+}
