cdbd86633f320f7bf69b3789c5162328e46ab389
Adil <Adiljan.anwar@gmail.com>
2022-08-31 17:54:08 +0300

    Bean life cycle

--- src/com/atguigu/spring5/testDemo/TestSpring5Demo1.java
+++ src/com/atguigu/spring5/testDemo/TestSpring5Demo1.java
@@ -1,7 +1,10 @@
 package com.atguigu.spring5.testDemo;
 
+import com.atguigu.spring5.bean.Orders;
 import com.atguigu.spring5.collectionType.Book;
+import com.atguigu.spring5.collectionType.Course;
 import com.atguigu.spring5.collectionType.Stu;
+import com.atguigu.spring5.factoryBean.MyBean;
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -23,5 +26,27 @@ public class TestSpring5Demo1 {
                 new ClassPathXmlApplicationContext("bean2.xml");
         Book book = context.getBean("book", Book.class);
         book.test();
+    }
+
+    @Test
+    public void test3(){
+        ApplicationContext context =
+                new ClassPathXmlApplicationContext("bean3.xml");
+        Course course = context.getBean("myBean", Course.class);
+        System.out.println(course);
+    }
+
+    @Test
+    public void testBean3(){
+//        ApplicationContext context =
+//                new ClassPathXmlApplicationContext("bean4.xml");
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("bean4.xml");
+        Orders orders = context.getBean("orders", Orders.class);
+        System.out.println("第四步 获取创建bean实例对象");
+        System.out.println(orders);
+
+        //手动让bean实例销毁
+        context.close();
     }
 }
